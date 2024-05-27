6139f05ea17e097eb8e082541b3146917fc47a64
Soufiane4906 <anibasoufiane2001@gmail.com>
2024-05-27 00:38:49 +0000

    New 26 crud is here

--- src/main/java/com/dailycodework/sbend2endapplication/home/HomeController.java
+++ src/main/java/com/dailycodework/sbend2endapplication/home/HomeController.java
@@ -1,8 +1,13 @@
 package com.dailycodework.sbend2endapplication.home;
 
+import com.dailycodework.sbend2endapplication.services.CharityActionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.text.AttributedString;
 
 /**
  * @author Sampson Alfred
@@ -10,10 +15,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class HomeController {
+    @Autowired
+    private final CharityActionService charityActionService;
+
+    public HomeController(CharityActionService charityActionService) {
+        this.charityActionService = charityActionService;
+    }
+
     @GetMapping
-    public String homePage(){
+    public String homePage(Model mode){
+
+        mode.addAttribute("charityActions", charityActionService.getAllCharityActions());
+
         return "home";
     }
+
     @GetMapping("/login")
     public String login(){
         return "login";
